0e500247f6f3a8ade65c155e9920ff9e5ba469d2
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-07 05:16:59 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  # def name=(new_name)
+  #   @name = new_name
+  # end
+end
+
+
+# beyonce = Person.new
+# beyonce.name = "Beyonce"

25df1ab41c69fb893f4d8241ce2a520e3b19e06b
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-08 03:21:18 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,56 @@
+
+
+
+
+# person1 =
+
+# class Person
+#   def initialize (name, job)
+#     @name = name
+#     @job = job
+#   end
+#
+#   def beyonce= name
+#      @name
+#      @job
+#   end
+#
+#   # def name=(new_name)
+#   #   @name = new_name
+#   # end
+# end
+#
+# beyonce = Person.new("Beyonce", "Singer")
+#
+# beyonce.name
+
+
+
 class Person
-  def initialize(name)
+
+  def person(name, job)
     @name = name
+    @job = job
   end
-  
+
   def name
     @name
   end
-  
-  # def name=(new_name)
-  #   @name = new_name
-  # end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+
+  def name=(new_name)
+    @name = new_name
+  end
+
 end
 
-
-# beyonce = Person.new
-# beyonce.name = "Beyonce"
+beyonce = Person.new
+beyonce.name
